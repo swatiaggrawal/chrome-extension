5cc3fb49da5dcce15c65637043097c48d0a82e67
Swati Aggrawal <swatiagg357@gmail.com>
2022-08-28 21:05:22 +0530

    update

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 
-let inputBtn=document.getElementById("input-btn")
+let myLeads=["w","e","y"]
+const inputEl=document.getElementById("input-el")
+const inputBtn=document.getElementById("input-btn")
+const ulEl=document.getElementById("ul-el")
 inputBtn.addEventListener("click",function(){
-    console.log("hi")
+    myLeads.push(inputEl.value)
+    console.log(myLeads)
 })
+let listItems=""
+
+for(let i=0;i<myLeads.length;i++){
+   //ulEl.innerHTML+="<li>"+myLeads[i]+"</li>"
+
+   //const li=document.createElement("li")
+   //li.textContent=myLeads[i]
+   //ulEl.append(li)
+
+   listItems+="<li>"+myLeads[i]+"</li>"
+   //console.log(listItems)
+
+}
+ulEl.innerHTML=listItems
