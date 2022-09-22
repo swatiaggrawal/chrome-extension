affd1800ca51266b18cc44b7beddf306a9566eda
Swati Aggrawal <swatiagg357@gmail.com>
2022-09-23 00:12:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,34 +10,36 @@ const leadFromlocalStorage=JSON.parse(localStorage.getItem("myLeads"))
 
 if(leadFromlocalStorage){
     myLeads=leadFromlocalStorage
-    renderLeads()
+    render(myLeads)
 }
+function render(leads){
+    let listItems=""
+    
+    for(let i=0;i<leads.length;i++){
+    listItems+= `
+                    <li>
+                        <a href='${leads[i]}' target='_blank' >
+                            ${leads[i]}
+                        </a>
+                    </li>
+                `
+    }
+    ulEl.innerHTML=listItems
+}
+
+
 deleteBtn.addEventListener("dblclick",function(){
     localStorage.clear()
     myLeads=[]
-    renderLeads()
+    render(myLeads)
 })
 
 inputBtn.addEventListener("click",function(){
     myLeads.push(inputEl.value)
     inputEl.value=""
     localStorage.setItem("myLeads",JSON.stringify(myLeads))
-    renderLeads()
+    render(myLeads)
    // console.log( localStorage.getItem("myLeads") )
     
     
 })
-function renderLeads(){
-let listItems=""
-
-for(let i=0;i<myLeads.length;i++){
-listItems+= `
-                <li>
-                    <a href='${myLeads[i]}' target='_blank' >
-                        ${myLeads[i]}
-                    </a>
-                </li>
-            `
-}
-ulEl.innerHTML=listItems
-}
